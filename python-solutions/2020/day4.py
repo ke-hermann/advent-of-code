cbfc689dd02870c1d652813d8af888f9ef8e497a
Kevin Hermann <hermannkevin@gmail.com>
2022-12-09 22:37:58 +0100

    2020 day4

--- python-solutions/2020/day4.py
+++ python-solutions/2020/day4.py
@@ -1,16 +1,30 @@
-with open("./resources/2020/day4.txt", "r") as infile:
-    data = infile.read().split("\n\n")
-    entries = []
-    for e in data:
-        s = e.replace("\n", " ")
-        xs = dict([t.split(":") for t in s.split()])
-        entries.append(xs)
+# Part 1
+import re
 
-required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-total = 0
+with open('./resources/2020/day4.txt', 'r') as file:
+    lst = file.read().split('\n\n')
+    lst = [x.replace('\n', ' ').split() for x in lst]
+    passports = []
+    for person in lst:
+        passports.append(dict(data.split(':') for data in person))
 
-for entry in entries:
-    valid = all(x in entry.keys() for x in required)
-    total += valid
+    passports = [x for x in passports if len(x.keys()) == 8 or (len(x) == 7 and 'cid' not in x.keys())]
+    print(len(passports))
 
-print(total)
+    # Part 2
+    valid_passports = []
+
+    values = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    for person in passports:
+        if (1920 <= int(person['byr']) <= 2002
+                and (2010 <= int(person['iyr']) <= 2020)
+                and (2020 <= int(person['eyr']) <= 2030)
+                and
+                ((person['hgt'][-2:] == 'cm' and 150 <= int(person['hgt'][:-2]) <= 193)
+                 or (person['hgt'][-2:] == 'in' and 59 <= int(person['hgt'][:-2]) <= 76))
+                and (re.match(r'#[\da-f]{6}', person['hcl']))
+                and (person['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])
+                and (re.match(r'\d{9}', person['pid']))):
+            valid_passports.append(person)
+
+print(len(valid_passports) - 1)
