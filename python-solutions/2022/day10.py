67c029326dc1b8c482425b1e20113f2578fd9d6d
Kevin Hermann <hermannkevin@gmail.com>
2022-12-10 07:15:07 +0100

    fixes

--- python-solutions/2022/day10.py
+++ python-solutions/2022/day10.py
@@ -19,31 +19,28 @@ class VirtualMachine:
             case ["addx", v]:
                 self.task_queue[self.cycle + 2].append(int(v))
                 self.step(2)
-                
 
     def step(self, count=1):
         for _ in range(count):
             if self.cycle in self.task_queue:
                 for i in self.task_queue[self.cycle]:
                     self.x += i
-            # part 1
-            if self.cycle in [20, 60, 100, 140, 180, 220]:
-                self.signal_strength += self.x * self.cycle
-            # part 2, draw to screen 
+            # part 2, draw to screen
             if self.cycle < 240:
                 row = self.cycle // 40
                 col = self.cycle % 40
                 if self.x - 1 <= col <= self.x + 1:
                     self.crt_screen[row][col] = "#"
             self.cycle += 1
-
+            # part 1
+            if self.cycle in [20, 60, 100, 140, 180, 220]:
+                self.signal_strength += self.x * self.cycle
 
     def run(self):
         while self.instructions:
             self.exec_instruction()
         while self.cycle <= max(self.task_queue.keys()):
             self.step()
-            
 
 
 with open("./resources/day10.txt", "r") as infile:
