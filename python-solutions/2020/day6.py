1f6a75426f13755beebd7b00f551502239677daa
Kevin Hermann <hermannkevin@gmail.com>
2022-12-10 02:11:57 +0100

    2020 day6

---
+++ python-solutions/2020/day6.py
@@ -0,0 +1,22 @@
+from functools import reduce
+
+with open("resources/2020/day6.txt", "r") as infile:
+    data = infile.read().split("\n\n")
+    groups = []
+    for entry in data:
+        groups.append(entry.split("\n"))
+
+# part 1 
+total = 0
+for group in groups:
+    distinct = len(set("".join(group)))
+    total += distinct
+print(total)
+
+# part 2 
+total_2 = 0
+for group in groups:
+    s = [set(p) for p in group]
+    everyone = reduce(set.intersection, s)
+    total_2 += len(everyone)
+print(total_2)
