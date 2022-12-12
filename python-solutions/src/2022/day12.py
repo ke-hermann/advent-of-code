077ecb52939a8e0c21c0cb70ab85a366c81641a6
Kevin Hermann <hermannkevin@gmail.com>
2022-12-12 08:52:24 +0100

    2022 day12 p2

--- python-solutions/src/2022/day12.py
+++ python-solutions/src/2022/day12.py
@@ -2,26 +2,35 @@ import networkx as nx
 
 with open("./resources/2022/day12.txt", "r") as infile:
     data = infile.read().splitlines()
-    heights = {}
+    m = {}
     G = nx.DiGraph()
 
     for i, row in enumerate(data):
         for j, v in enumerate(row):
-            heights[(i, j)] = v
-        
-    target = [k for k in heights.keys() if heights[k] == "E"][0]
-    start = [k for k in heights.keys() if heights[k] == "S"][0]
-    heights[target] = "z"
-    heights[start] = "s"
+            m[(i, j)] = v
 
-    for (x, y) in heights.keys():
-        adjacent = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-        adjacent = [p for p in adjacent if p in heights.keys()]
-        adjacent = [
-            p for p in adjacent if (ord(heights[p]) - ord(heights[(x, y)])) <= 1
-        ]
-        for a in adjacent:
-            G.add_edge((x, y), a)
+    target = [k for k in m.keys() if m[k] == "E"][0]
+    start = [k for k in m.keys() if m[k] == "S"][0]
+    m[target] = "z"
+    m[start] = "s"
 
-    result = nx.shortest_path(G, start, target)
-    print(target, len(result))
+for (x, y) in m.keys():
+    adjacent = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+    adjacent = [p for p in adjacent if p in m.keys()]
+    adjacent = [p for p in adjacent if (ord(m[p]) - ord(m[(x, y)])) <= 1]
+    for a in adjacent:
+        G.add_edge((x, y), a)
+
+# part 1
+result = nx.shortest_path(G, start, target)
+print(len(result) - 1)
+
+# part 2
+candidates = [k for k in m.keys() if m[k] == "a"]
+paths = []
+for c in candidates:
+    if nx.has_path(G, c, target):
+        result = nx.shortest_path(G, c, target)
+        paths.append(len(result) - 1)
+
+print(min(paths))
