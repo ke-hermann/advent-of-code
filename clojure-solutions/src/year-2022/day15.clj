6a8830cc0f8e1b76a06a7dfe63b95ad8c8dd469d
Kevin Hermann <hermannkevin@gmail.com>
2022-12-15 09:43:23 +0100

    work on p2

--- clojure-solutions/src/year-2022/day15.clj
+++ clojure-solutions/src/year-2022/day15.clj
@@ -23,12 +23,22 @@
 (defn in-range? [i xs]
   (some (fn [[x y]] (and (>= i x) (<= i y))) xs))
 
-(defn part-1 []
-  (let [t 2000000
-        el (remove empty? (map (partial eliminated t) input))
-        l (apply min (map first el))
-        r (apply max (map second el))
-        b (map first (filter (fn [[x y]] (= y t)) beacons))]
-    (loop [i l c 0]
-      (if (= i r) c
-          (recur (inc i) (if (in-range? i el) (inc c) c))))))
+(defn merge-pairs [pairs]
+  (loop [[x y] (first pairs) result [] q (rest pairs)]
+    (let [[i j] (first q)]
+      (cond
+        (empty? q) (conj result [x y])
+        (< y i)  (recur [i j] (conj result [x y]) (rest q))
+        (>= y j) (recur [x y] result (rest q))
+        (>= y i) (recur [x j] result (rest q))))))
+
+(defn part-1 [row]
+  (let [el (remove empty? (map (partial eliminated row) input))
+        no (merge-pairs (sort-by first el))]
+    no))
+
+(defn part-2 []
+  (loop [row 0 results []]
+    (if (= row 4000000)
+      (filter #(> (count %) 1) results)
+      (recur (inc row) (conj results (part-1 row))))))
