4f085b1064c0aa1020a93b5aab2835c811326a8a
Kevin Hermann <hermannkevin@gmail.com>
2022-12-20 08:00:45 +0100

    2022 day 20 p2

--- python-solutions/src/2022/day20.py
+++ python-solutions/src/2022/day20.py
@@ -2,10 +2,11 @@ from collections import deque
 
 with open("../../resources/2022/day20.txt", "r") as infile:
     data = infile.read().splitlines()
-    data = list(enumerate([int(x) for x in data]))
+    data = list(enumerate([int(x) * 811589153 for x in data]))
     mixed = deque(data[:])
     l = len(data)
 
+for _ in range(10):
     for p in data:
         i = mixed.index(p)
         mixed.rotate(-i)
@@ -15,7 +16,4 @@ with open("../../resources/2022/day20.txt", "r") as infile:
 
 r = [x[1] for x in mixed]
 z = r.index(0)
-total = 0
-for i in [1000, 2000, 3000]:
-    total += r[(z + i) % l]
-print(total)
+print(sum([r[(z + i) % l]for i in [1000, 2000, 3000]]))
