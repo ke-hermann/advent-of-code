85768ca4ff54cabbb4a51f9e48af98bac9b9313c
Kevin Hermann <hermannkevin@gmail.com>
2022-12-21 09:34:11 +0100

    2022 day21 p2

--- python-solutions/src/2022/day21.py
+++ python-solutions/src/2022/day21.py
@@ -16,6 +16,12 @@ class Monkey:
 
 with open("resources/2022/day21.txt", "r") as infile:
     data = infile.read().splitlines()
+    testing = True
+    low = 0
+    high = 9999999999999999
+
+for i in range(50):
+
     monkeys = {}
     for line in data:
         s = line.replace(":", "").split()
@@ -24,17 +30,25 @@ with open("resources/2022/day21.txt", "r") as infile:
         else:
             monkeys[s[0]] = Monkey(*s[1:])
 
+    monkeys["humn"] = Monkey(None, None, None, (low + high) // 2)
+
+
     while monkeys["root"].out is None:
         for name, m in monkeys.items():
             if m.out is not None:
                 continue 
-            if type(m.x) == str and  monkeys[m.x].out:
+            if type(m.x) == str and monkeys[m.x].out:
                 m.x = monkeys[m.x].out
             if type(m.y) == str and monkeys[m.y].out:
                 m.y = monkeys[m.y].out
 
             if isinstance(m.x, Number) and isinstance(m.y, Number):
-                m.out = op[m.f](m.x, m.y) 
+                m.out = op[m.f](m.x, m.y)
 
-    print(monkeys["root"])
-        
+    if monkeys["root"].out > 0:
+        low = (low + high) // 2
+    else:
+        high = (low + high) // 2
+
+    r = monkeys["root"]
+    print(low, high, abs(r.x - r.y))
