15937065ea549b9784d42706f4ad0cd4cae4fc9b
Kevin Hermann <hermannkevin@gmail.com>
2022-12-21 06:44:48 +0100

    autosave

--- python-solutions/src/2022/day19.py
+++ python-solutions/src/2022/day19.py
@@ -1,50 +1,41 @@
-from collections import defaultdict
-import re
+import fileinput, re
 
-with open("resources/2022/day19.txt", "r") as infile:
-    data = infile.read().splitlines()
-    blueprints = []
-    for line in data:
-        xs = re.findall("\d+", line)
-        xs = [int(x) for x in xs[1:]]
-        blueprints.append({"ore": xs[0], "clay": xs[1], "obsidian": (xs[2], xs[3]), "geode": (xs[4], xs[5])})
-
-        
-for b in blueprints:
-    bots = ["ore"]
-    stash =  defaultdict(int)
-    minutes = 0
-    queue = []
-
-    while minutes <= 24:
-        queue = [(k, v - 1) for (k, v) in queue]
-        bots.extend([k for (k, v) in queue if v == 0])
-        queue = [x for x in queue if x[1] > 0]
-
-        if stash["ore"] >= b["geode"][0] and stash["obsidian"] >= b["geode"][1]:
-            queue.append(("geode", 1))
-            stash["ore"] -= b["geode"][0]
-            stash["obsidian"] -= b["geode"][1]
-
-        elif stash["ore"] >= b["obsidian"][0] and stash["clay"] >= b["obsidian"][1]:
-            queue.append(("obsidian", 1))
-            stash["ore"] -= b["obsidian"][0]
-            stash["clay"] -= b["obsidian"][1]
-
-        elif stash["ore"] >= b["clay"] and bots.count("clay") <= 3 * bots.count("ore"):
-            queue.append(("clay", 1))
-            stash["ore"] -= b["clay"]
-
-        elif stash["ore"] >= b["ore"]:
-            queue.append(("ore", 1))
-            stash["ore"] -= b["ore"]
-
-        stash["ore"] += bots.count("ore")
-        stash["clay"] += bots.count("clay")
-        stash["obsidian"] += bots.count("obsidian")
-        stash["geode"] += bots.count("geode")
-
-        minutes += 1
-
-
-    print(stash)
+r = 1
+o = [ ( t - 1 ) * t // 2 for t in range( 32 + 1 ) ]
+p = [ list( map( int, re.findall( "-?\d+", l ) ) ) for l in fileinput.input() ]
+for n, a, b, c, d, e, f in p[ : 3 ]:
+    m = 0
+    mi, mj, mk = max( a, b, c, e ), d, f
+    def dfs( t, g,         # t:time remaining, g:goal robot type
+             i, j, k, l,   # i:ore, j:clay, k:obsidian, l:geode robots
+             w, x, y, z ): # w:ore, x:clay, y:obsidian, z:geode available
+        global m
+        if ( g == 0 and i >= mi or
+             g == 1 and j >= mj or
+             g == 2 and ( k >= mk or j == 0 ) or
+             g == 3 and k == 0 or
+             z + l * t + o[ t ] <= m ):
+            return
+        while t:
+            if g == 0 and w >= a:
+                for g in range( 4 ):
+                    dfs( t - 1, g, i + 1, j, k, l, w - a + i, x + j, y + k, z + l )
+                return
+            elif g == 1 and w >= b:
+                for g in range( 4 ):
+                    dfs( t - 1, g, i, j + 1, k, l, w - b + i, x + j, y + k, z + l )
+                return
+            elif g == 2 and w >= c and x >= d:
+                for g in range( 4 ):
+                    dfs( t - 1, g, i, j, k + 1, l, w - c + i, x - d + j, y + k, z + l )
+                return
+            elif g == 3 and w >= e and y >= f:
+                for g in range( 4 ):
+                    dfs( t - 1, g, i, j, k, l + 1, w - e + i, x + j, y - f + k, z + l )
+                return
+            t, w, x, y, z = t - 1, w + i, x + j, y + k, z + l
+        m = max( m, z )
+    for g in range( 4 ):
+        dfs( 32, g, 1, 0, 0, 0, 0, 0, 0, 0 )
+    r *= m
+print( r )
