ed535d8ce9e084489662836b6ddf8ed16ff3d930
Kevin Hermann <hermannkevin@gmail.com>
2022-12-22 22:30:14 +0100

    2022 day 22 p2 edge_walk logic preliminary

--- python-solutions/src/2022/day22.py
+++ python-solutions/src/2022/day22.py
@@ -4,6 +4,44 @@ TURN_R = {"L": "U", "D": "L", "R": "D", "U": "R"}
 from copy import deepcopy
 
 
+def walk_edges(x, y, fcg):
+    if 150 <= x <= 199 and 0 <= y <= 49: # surface 1 
+        if fcg == "R":
+            return (149, (x - 150) + 50, "U")
+        elif  fcg == "D":
+            return (x - 150, y + 100, "D")
+        elif fcg == "L":
+            return (0, (x - 150) + 50, "D")
+    elif 100 <= x <= 149 and 0 <= y <= 49: # surface 2 
+        if fcg == "L":
+            return (50 + (x - 100), 0, "R")
+        elif fcg == "U":
+            return (y + 50, 50, "R")
+    elif 100 <= x <= 149 and 50 <= y <= 99: # surface 3
+        if fcg == "D":
+            return ((y - 50) + 150, 49, "L")
+        elif fcg == "R":
+            x_off = x - 100
+            return (49 - x_off, 149, "L")
+    elif 50 <= x <= 99 and 50 <= y <= 99: # surface 4 
+        if fcg == "L":
+            return (100 ,(x - 50), "D")
+        elif fcg == "R":
+            return (49 , (x - 50) + 100, "U")
+    elif 0 <= x <= 49 and 50 <= y <= 99: # surface 5
+        if fcg == "L":
+            return (149 - x , 0, "R")
+        elif fcg == "U":
+            return (150 + (y - 50), 0, "R")
+    elif 0 <= x <= 49 and 100 <= y <= 149: # surface 6
+        if fcg == "U":
+            return (199, y - 100, "U")
+        elif fcg == "R":
+            return (149 - x, 99, "L")
+        elif fcg == "D":
+            return ((y - 100) + 50, 99, "L")
+
+
 with open("resources/2022/day22_moves.txt", "r") as moves_file:
     # had to manually add zero move section to input due to scuffed parsing
     # further down below
@@ -70,3 +108,5 @@ for c in MOVES:
 x += 1
 y += 1 
 print(x * 1000 + 4 * y + {"R": 0, "D": 1, "L": 2, "U": 3}[facing])
+
+print((149, 50) in board)
