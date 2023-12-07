ad157584e21090cc442de123e4bfc4c85b6f3e3d
Kevin Hermann <hermannkevin@gmail.com>
2023-12-07 13:56:06 +0100

    2023 day7 p1

---
+++ src/advent_of_code/year_2023/day7.clj
@@ -0,0 +1,59 @@
+(ns advent-of-code.year-2023.day7
+  (:require [clojure.string :as str]))
+
+
+(def input (->> (slurp "resources/2023/day7.txt")
+                (str/split-lines)
+                (map (fn [s] (str/split s #"\s+")))
+                (map (fn [[k v]] [k (parse-long v)]))
+                (into {})))
+
+(def card-vals (into {} (map vector "23456789ATJQKA" (range))))
+
+(defn five-of-a-kind? [hand]
+  (.contains (vals (frequencies hand)) 5))
+
+(defn four-of-a-kind? [hand]
+  (.contains (vals (frequencies hand)) 4))
+
+(defn full-house? [hand]
+  (= '(2 3) (sort (vals (frequencies hand)))))
+
+(defn three-of-a-kind? [hand]
+  (= '(1 1 3) (sort (vals (frequencies hand)))))
+
+(defn two-pair? [hand]
+  (= '(1 2 2) (sort (vals (frequencies hand)))))
+
+(defn one-pair? [hand]
+  (= '(1 1 1 2) (sort (vals (frequencies hand)))))
+
+(defn high-card? [hand]
+  (apply distinct? hand))
+
+(defn score [hand]
+  (cond
+    (five-of-a-kind? hand) 7
+    (four-of-a-kind? hand) 6
+    (full-house? hand) 5
+    (three-of-a-kind? hand) 4
+    (two-pair? hand) 3
+    (one-pair? hand) 2
+    (high-card? hand) 1
+    :else 0))
+
+(defn compare-hands [hand-1 hand-2]
+  (let [x (score hand-1)
+        y (score hand-2)]
+    (if (not= x y)
+      (compare x y)
+      (loop [[[a b] & more] (map vector hand-1 hand-2)]
+        (let [s (compare (card-vals a) (card-vals b))]
+          (if (zero? s)
+            (recur more) s))))))
+
+(defn part-1 []
+  (->> (sort compare-hands (keys input))
+       (map vector (range 1 (inc (count input))))
+       (map (fn [[i k]] (* i (input k))))
+       (reduce +)))
