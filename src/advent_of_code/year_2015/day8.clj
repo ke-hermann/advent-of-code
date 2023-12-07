c964579f52fdc8e43ba73722d3d5bdf55af9214b
Kevin Hermann <hermannkevin@gmail.com>
2023-12-07 02:05:29 +0100

    2015 day 8 part 2

--- src/advent_of_code/year_2015/day8.clj
+++ src/advent_of_code/year_2015/day8.clj
@@ -9,19 +9,22 @@
   (str/replace s #"\\x[0-9a-f]{2}" "?"))
 
 (defn rep-back [s]
-  (str/replace s #"\\\\" "1"))
+  (str/replace s #"\\" "??"))
 
 (defn rep-quote [s]
-  (str/replace s #"\\\"" "\""))
+  (str/replace s #"\"" "??"))
 
 (defn rep-all [s]
-  (-> (rep-hex s)
-      (rep-quote)
+  (-> (rep-quote s)
       (rep-back)))
 
 (defn length [s]
-  (- (count (rep-all s)) 2))
+  (+ (count (rep-all s)) 2))
 
 (defn part-1 []
   (- (reduce + (map count input))
      (reduce + (map length input))))
+
+(defn part-2 []
+  (-(reduce + (map length input))
+    (reduce + (map count input))))
