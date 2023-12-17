1f4974b9e9283ce35c61e2fe040140324f4116a9
Kevin Hermann <hermannkevin@gmail.com>
2023-12-17 03:02:35 +0100

    more old puzzles

--- src/advent_of_code/year_2015/day11.clj
+++ src/advent_of_code/year_2015/day11.clj
@@ -1,4 +1,32 @@
-(ns advent-of-code.year-2015.day11)
+(ns advent-of-code.year-2015.day11
+  (:require [clojure.string :as str]))
 
+(defn inc-char [c]
+  (char (inc (int c))))
 
-(def input "asdf")
+(defn inc-string [string]
+  (loop [s string i (dec (count string))]
+    (let [c (nth s i nil)]
+      (cond
+        (= i -1) (str "a" s)
+        (not= c \z) (str/join (assoc (vec s) i (inc-char c)))
+        :else (recur (str/join (assoc (vec s) i \a)) (dec i) )))))
+
+(defn straight? [s]
+  (some (fn [[a b c]] (= (+ 2 (int a)) (inc (int b)) (int c))) (partition 3 1 s)))
+
+(defn no-forbidden? [s]
+  (empty? (filter #{\i \o \l} s)))
+
+(defn pairs? [s]
+  (->> (partition-by identity s)
+       (filter #(= 2 (count %)))
+       (#(= 2 (count %)))))
+
+(defn password? [s]
+  (and (straight? s) (no-forbidden? s) (pairs? s)))
+
+(defn part-1 []
+  (loop [s (inc-string "hepxxyzz")]
+    (if (password? s) s
+        (recur (inc-string s)))))
--- src/advent_of_code/year_2015/day7.clj
+++ src/advent_of_code/year_2015/day7.clj
@@ -1,24 +1,65 @@
 (ns advent-of-code.2015.day7
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [clojure.core.match :refer [match]]))
 
 
 (def input (->> (slurp "resources/2015/day7.txt")
                 (s/split-lines)
                 (map #(s/split % #"\s+"))))
 
-(defn lookup [m k]
-  (get m k (read-string k)))
+(defn num? [s] (number? (read-string s)))
 
-(defn step [m instruction]
-  (let [[a b c d e] instruction]
-    (cond
-      (= b "->") (assoc m c (read-string a))
-      (= b "AND") (assoc m e (bit-and (lookup m a) (lookup m c)))
-      (= b "OR") (assoc m e (bit-or (lookup m a) (lookup m c)))
-      (= b "LSHIFT") (assoc m e (bit-shift-left (lookup m a) (lookup m c)))
-      (= b "RSHIFT") (assoc m e (bit-shift-right (lookup m a) (lookup m c)))
-      (= a "NOT") (assoc m d (bit-and-not 16rFFFF (lookup m b)))
-      :else (throw (Exception. "malformed m")))))
+(defn shift-right [vm k1 k2 n]
+  (if (nil? (vm k1))
+    :stalled
+    (assoc vm k2 (bit-shift-right (vm k1) (parse-long n)))))
 
-(defn solve []
-  (reduce step {} input))
+(defn shift-left [vm k1 k2 n]
+  (if (nil? (vm k1))
+    :stalled
+    (assoc vm k2 (bit-shift-left (vm k1) (parse-long n)))))
+
+(defn or-op [vm x y k]
+  (let [a (if (num? x) (parse-long x) (vm x))
+        b (if (num? y) (parse-long y) (vm y))]
+    (if (or (nil? a) (nil? b))
+      :stalled
+      (assoc vm k (bit-or a b)))))
+
+
+(defn and-op [vm x y k]
+  (let [a (if (num? x) (parse-long x) (vm x))
+        b (if (num? y) (parse-long y) (vm y))]
+    (if (or (nil? a) (nil? b))
+      :stalled
+      (assoc vm k (bit-and a b)))))
+
+(defn set-op [vm k v]
+  (let [v (if (num? v) (parse-long v) (vm v))]
+    (if (nil? v)
+      :stalled
+      (assoc vm k v))))
+
+(defn not-op [vm k v]
+  (let [v (if (num? v) (parse-long v) (vm v))]
+    (if (nil? v)
+      :stalled
+      (assoc vm k (bit-and-not 16rFFFF v)))))
+
+(defn step [vm instruction]
+  (match instruction
+         [v "->" k] (set-op vm k v)
+         [k1 "RSHIFT" n "->" k2] (shift-right vm k1 k2 n)
+         [k1 "LSHIFT" n "->" k2] (shift-left vm k1 k2 n)
+         [x "OR" y "->" k] (or-op vm x y k)
+         ["NOT" v "->" k] (not-op vm k v)
+         [x "AND" y "->" k] (and-op vm x y k)
+         :else (throw (Error. "missing clause"))))
+
+(defn process []
+  (loop [vm {} [hd & tl :as instructions] input]
+    (if (empty? instructions) (vm "a")
+        (let [vm* (step vm hd)]
+          (if (= :stalled vm*)
+            (recur vm (conj (vec tl) hd))
+            (recur  vm* (vec tl)))))))
