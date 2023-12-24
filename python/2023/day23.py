235f4fe7bf0f540d08d4dba9e9a9a5ee604baf14
Kevin Hermann <hermannkevin@gmail.com>
2023-12-24 06:03:46 +0100

    2023 day23 p2

--- python/2023/day23.py
+++ python/2023/day23.py
@@ -1,47 +1,53 @@
-import rustworkx as rx
+START = (0, 1)
+END = (140, 139)
+
+
+def intersect_dist(cur, dist, seen):
+    if cur in intersections:
+        return (cur, dist)
+
+    n = [p for p in neighbors[cur] if p not in seen][0]
+    return intersect_dist(n, dist + 1, seen | {cur})
+
+
+def bfs(node, score, seen):
+    if node == END:
+        yield score
+
+    for p, dist in graph[node]:
+        if p in seen:
+            continue
+        yield from bfs(p, score + dist, seen | {node})
 
 
 with open("./day23.in", "r") as infile:
     data = infile.read().splitlines()
-    G = rx.PyGraph()
-    indices = {}
+    neighbors = {}
+    intersections = [START, END]
+    graph = {}
 
     for i, row in enumerate(data):
         for j, val in enumerate(row):
             if val not in ".><v^":
                 continue
+            ec = 0
 
-            idx_from = None
-            if (i, j) not in indices:
-                idx_from = G.add_node((i, j))
-                indices[(i, j)] = idx_from
-            else:
-                idx_from = indices[(i, j)]
-
-            for (x, y) in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
+            for x, y in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
                 m, n = i + x, j + y
                 if m < 0 or m >= len(data) or n < 0 or n >= len(data[0]):
                     continue
                 if data[m][n] not in ".><v^":
                     continue
+                ec += 1
 
-                idx_to = None
-                if (m, n) not in indices:
-                    idx_to = G.add_node((m, n))
-                    indices[(m, n)] = idx_to
-                else:
-                    idx_to = indices[(m, n)]
+                neighbors.setdefault((i, j), []).append((m, n))
+            if ec >= 3:
+                intersections.append((i, j))
 
-                if (idx_from, idx_to) and (idx_to, idx_from) not in G.edge_list():
-                    G.add_edge(idx_from, idx_to, None)
+    for i in intersections:
+        for n in neighbors[i]:
+            (t, d) = intersect_dist(n, 1, {i})
+            graph.setdefault(i, []).append((t, d))
 
-
-    longest_path = 0
-    result = rx.all_simple_paths(G, 0, 9371)
-    for r in result:
-        l = len(r)
-        if l > longest_path:
-            longest_path = l
-            print(l)
-
-    print(longest_path - 1)
+    result = bfs(START, 0, set([START]))
+    print(max(result))
