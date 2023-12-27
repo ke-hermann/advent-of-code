684b89dd3bf6517f2f951fa5e9ff25ed428cad02
Kevin Hermann <hermannkevin@gmail.com>
2023-12-27 07:39:35 +0100

    2017 day24 p1

---
+++ python/2017/day24.py
@@ -0,0 +1,38 @@
+def bridge(seq: list, ports: set):
+    if not ports:
+        yield seq
+
+    no_matches = True
+    for a, b in ports:
+        if seq[-1] == a:
+            yield from bridge(seq + [b], ports - {(a, b)})
+            no_matches = False
+        if seq[-1] == b:
+            yield from bridge(seq + [a], ports - {(a, b)})
+            no_matches = False
+
+    if no_matches:
+        total = 0
+        for i in range(len(seq) - 1):
+            total += seq[i] + seq[i + 1]
+        yield total
+
+
+with open("./day24.txt", "r") as infile:
+    data = infile.read().splitlines()
+    ports = []
+
+    for line in data:
+        a, b = line.split("/")
+        ports.append((int(a), int(b)))
+
+    score = 0
+    starts = [p for p in ports if p[0] == 0 or p[1] == 0]
+    for s in starts:
+        ports = [p for p in ports if p != s]
+        start = [s[0], s[1]] if s[0] == 0 else [s[1], s[0]]
+        results = bridge(start, set(ports[:]))
+        m = max(results)
+        score = m if m > score else m
+
+    print(score)
