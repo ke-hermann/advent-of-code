42695dea3b93836f2c17a6a064a097669a5da4cc
Kevin Hermann <hermannkevin@gmail.com>
2025-12-10 08:53:56 +0100

    aoc 2025 day10 p2

--- python/2025/day10.py
+++ python/2025/day10.py
@@ -1,12 +1,33 @@
 import re 
+from itertools import permutations, product
+import sys
 
 
 with open("day10.txt", "r") as infile:
     data = infile.read().splitlines()
+    presses = 0
+
     for line in data:
-        print(line)
-        indicators = re.findall(r"\[.*?\]", line)
+        indicators = list(re.findall(r"\[.*?\]", line)[0][1:-1])
         wiring = re.findall(r"\(.*?\)", line)
+        wiring = [[int(x) for x in s[1:-1].split(",")] for s in wiring]
         joltage = re.findall(r"\{.*?\}", line)
 
-        print(indicators, wiring, joltage)
+        i = 1
+        run = True
+
+        while run:
+            for p in product(wiring, repeat=i):
+                target = list("." * len(indicators))
+                for button in p:
+                    for x in button:
+                        target[x] = "#" if target[x] == "." else "."
+                if target == indicators:
+                    run = False
+                    presses += i
+                    break
+            else:
+                i += 1
+
+    print(presses)
+
